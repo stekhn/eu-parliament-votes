18c3609bd348194851c7b8b0e2275ca4bc5ee89f
Steffen Kühne <stekhn@gmail.com>
2019-11-26 16:31:05 +0100

    Update dependencies and replace node-sass with dart-sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -22,12 +23,21 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('sass'),
+            sourceMap: true
+          }
+        }]
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, './src/images'), to: './images' }
+    ]),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
